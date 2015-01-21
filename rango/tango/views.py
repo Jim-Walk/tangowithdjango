fe432d18b186969573482986170eac339053f461
Jim Walker <j.m.walker@live.co.uk>
2015-01-21 14:40:51 +0000

    Chapter 4 - completed

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -5,4 +5,5 @@ def index(request):
     return HttpResponse("Rango says hello world<br/> <a href='/rango/about/'>About</a>")
 
 def about(request):
-    return HttpResponse("Rango says this is the about page<br/> <a href='/rango/'>Index</a>")
+    return HttpResponse("Rango says this is the about page<br/>"
+                        " <a href='/rango/'>Index</a><br/>Made by Jim Walker<br/>2096502w")
